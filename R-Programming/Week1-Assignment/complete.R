bcb17e2943bfc267f2c1ccadaef78eb4902e1dc8
Toyo <toyogarcia@gmail.com>
2017-04-16 21:15:39 +0200

    Assignments

---
+++ R-Programming/Week1-Assignment/complete.R
@@ -0,0 +1,30 @@
+complete <- function(directory, id=1:332){
+###################################################################
+# Program: complete.R
+# Parameters:
+#   directory -> Path to the files
+#   id        -> Files to return the number of complete cases
+# Sample call:
+#   complete("C:/Coursera/Data Science/2.- R Programming/Week 1/Exercises/specdata",30:25)
+###################################################################
+    
+    # return the list of directory files
+    fileList <- list.files(directory, full.names = TRUE)
+    # intialize data frame to store the id and obs
+    totFileCC <- data.frame()
+    
+    # loop through the files
+    for (i in id){
+        # read the file
+        fileData  <- read.csv(fileList[i])
+        # get complete cases
+        compCases <- fileData[complete.cases(fileData), ]
+        # add row with id and obs to the data frame
+        totFileCC <- rbind(totFileCC, c(i,nrow(compCases)))
+    }
+    
+    # names the data frame columns
+    names(totFileCC) = c('id', 'nobs')
+    # returns the data frame
+    totFileCC
+}
